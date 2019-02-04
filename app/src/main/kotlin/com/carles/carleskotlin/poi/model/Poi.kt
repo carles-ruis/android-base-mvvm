66ee97d659a9040bade631eff13a9ae7f72c0d9f
carles ruiz <carles_ruis@hotmail.com>
2019-02-04 10:29:21 +0100

    update gitignore files

--- app/src/main/kotlin/com/carles/carleskotlin/poi/model/Poi.kt
+++ app/src/main/kotlin/com/carles/carleskotlin/poi/model/Poi.kt
@@ -3,7 +3,7 @@ package com.carles.carleskotlin.poi.model
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "poi")
 data class Poi(
     @PrimaryKey var id: String,
     var title: String? = null,
